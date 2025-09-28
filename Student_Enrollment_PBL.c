74106fea07522ddc0caf1843b11e61d979889ed9
HIMU-BUTOLA <himu11071980@gmail.com>
2025-09-28 17:43:45 +0530

    Update Student_Enrollment_PBL.c

--- Student_Enrollment_PBL.c
+++ Student_Enrollment_PBL.c
@@ -1,11 +1,3 @@
-/*Problem Statement 53: Course Enrollment System  
-Develop a C program for a Course Enrollment System to manage student enrollments and 
-course records efficiently. The system should include admin authentication to ensure secure 
-access for managing courses and enrollments. Key functionalities include adding course details 
-(course ID, name, instructor, credits), searching for courses by name or instructor, and sorting 
-courses by credits or alphabetical order. Additionally, it should allow removal of discontinued 
-courses and provide report generation on total courses offered and student enrollments. File 
-handling should be used to store and retrieve course records for data persistence. */
 #include <stdio.h>
 #include <string.h>
 #include "user_authentication_module.c"
@@ -48,4 +40,5 @@ int main() {
 
     return 0;
 }
+
 
